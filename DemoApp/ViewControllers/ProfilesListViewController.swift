e0b77bfd803acb9d1e5653526cf8e288c110e162
Marcin Srech <m.srech@tt.com.pl>
2018-01-13 18:28:00 +0100

    Implemented Profiles List VC.It has a table view and search bar. View model use apicliend and data provider

--- DemoApp/ViewControllers/ProfilesListViewController.swift
+++ DemoApp/ViewControllers/ProfilesListViewController.swift
@@ -7,12 +7,89 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ProfilesListViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    let cellIdentifier = "productCellIdentifier"
+    
+    var searchController: UISearchController!
+    
+    var profilesListViewModel: ProfilesListViewModel?
+    let disposeBag = DisposeBag()
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        profilesListViewModel = ProfilesListViewModel(dataProvider: CDProfileDataProvider())
 
+        prepareNavigation()
+        prepareTableView()
+        prepareSearchController()
+        
+        configureTableDataSource()
+        configureSearchResults()
+    }
+    
+    //MARK: - Prepare UI
+    private func prepareNavigation() {
+        title = profilesListViewModel?.getTitle()
+    }
+    
+    private func prepareTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    private func prepareSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.sizeToFit()
+        
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = false
+            navigationItem.searchController = searchController
+            navigationItem.hidesSearchBarWhenScrolling = false
+        } else {
+            tableView.tableHeaderView = searchController.searchBar
+        }
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        definesPresentationContext = true
     }
 
+    //MARK: - Configure Rx
+    private func configureTableDataSource() {
+        let observableProfiles = profilesListViewModel!.getProfiles().asObservable()
+        observableProfiles.bind(to: tableView.rx.items) { (tableView, row, profile) in
+                let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: self.cellIdentifier)
+                cell.textLabel?.text = profile.teacherNick!
+                cell.detailTextLabel?.text = profile.title!
+                cell.detailTextLabel?.numberOfLines = 0
+                cell.accessoryType = .disclosureIndicator
+                return cell
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
+    private func configureSearchResults() {
+        _ = searchController.searchBar.rx.text.orEmpty
+            .debounce(1, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .observeOn(MainScheduler.instance)
+            .subscribe({ [weak self] (event) in
+                self?.profilesListViewModel?.findProfiles(by: event.element)
+            })
+        
+        _ = searchController.searchBar.rx.cancelButtonClicked
+            .observeOn(MainScheduler.instance)
+            .subscribe({ [weak self] (event) in
+                self?.profilesListViewModel?.findProfiles(by: nil)
+            })
+    }
 }
--- DemoApp/ViewModels/ProfilesListViewModel.swift
+++ DemoApp/ViewModels/ProfilesListViewModel.swift
@@ -7,7 +7,46 @@
 //
 
 import Foundation
+import RxSwift
 
 class ProfilesListViewModel {
     
+    //MARK: - Properties
+    private let title = String.localized("ProfilesVC_title")
+    private var profiles = Variable<[CDProfile]>([])
+    private var pofilesDataProvider: CDProfileDataProvider
+    private var disposeBag = DisposeBag()
+    
+    //MARK: - Methods
+    init(dataProvider: DataProvider) {
+        self.pofilesDataProvider = dataProvider as! CDProfileDataProvider
+        fetchProductsAndUpdateObservableProducts()
+    }
+    
+    private func fetchProductsAndUpdateObservableProducts() {
+        pofilesDataProvider.fetchObservableData()
+            .map({ $0})
+            .subscribe({ (profiles) in
+                self.profiles.value = profiles.element as? [CDProfile] ?? []
+            })
+            .addDisposableTo(disposeBag)
+    }
+    
+    public func getTitle() -> String {
+        return title
+    }
+    
+    public func getProfiles() -> Variable<[CDProfile]> {
+        return profiles
+    }
+
+    public func findProfiles(by keywords: String?) {
+        if keywords == nil || keywords?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) == "" {
+            self.pofilesDataProvider.findObjects(by: "keyword", value: keywords ?? "")
+        } else {
+            APIClient.shared.findProducts(by: keywords!).always {
+                self.pofilesDataProvider.findObjects(by: "keyword", value: keywords!)
+            }
+        }
+    }
 }
